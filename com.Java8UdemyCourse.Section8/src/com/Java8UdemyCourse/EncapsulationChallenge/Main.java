4368fb6e900f17144a193c082e82cc7658a62599
Drithin <drithn@gmail.com>
2016-11-30 09:41:54 -0800

    feat: Encapsulation in Java Learned How to Encapsulate essential variables and importance of Encapsulation. Tim explained it live code demo. Took up the challenge exercise, coded the required methods and classes  for the challenge

---
+++ com.Java8UdemyCourse.Section8/src/com/Java8UdemyCourse/EncapsulationChallenge/Main.java
@@ -0,0 +1,15 @@
+package com.Java8UdemyCourse.EncapsulationChallenge;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Printer P = new Printer(100, true);
+
+		P.askToPrint(900);
+		
+		P.fillToner();
+		
+		
+	}
+
+}
