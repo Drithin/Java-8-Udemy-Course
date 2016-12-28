d1e80aad0e3212e02f8772a935dc290998f0e1e6
Drithin <drithn@gmail.com>
2016-12-28 11:28:51 -0800

    feat: Coded Main method to the challenge exercise

--- com.Java8UdemyCourse.Section8/src/com/Java8UdemyCourse/OOPSmasterchallenge/Main.java
+++ com.Java8UdemyCourse.Section8/src/com/Java8UdemyCourse/OOPSmasterchallenge/Main.java
@@ -3,8 +3,13 @@ package com.Java8UdemyCourse.OOPSmasterchallenge;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+		double price = hamburger.itemizeHamburger();
+	    hamburger.addHamburgerAddition1("Tomato", 0.27);
+		hamburger.addHamburgerAddition2("Lettuce", 0.75);
+		hamburger.addHamburgerAddition3("Cheese", 1.13);
+		System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+       }
 
 }
